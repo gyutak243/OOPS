6fd040bb692031fb7887847350b821dfb546ee6d
binaryhamin <jeonhm7450@gmail.com>
2026-05-23 07:50:58 +0900

    주석 추가

--- Team_Project/src/components/SettingProfile.jsx
+++ Team_Project/src/components/SettingProfile.jsx
@@ -30,7 +30,7 @@ const SettingProfile = () => {
             const reader = new FileReader();
             reader.onloadend = () => {
                 const imgResult = reader.result; 
-                setPreviewImg(imgResult); // 짠! 임시 주소로 이미지 State 교체 (화면 바뀜)
+                setPreviewImg(imgResult); //프로필 스테이트 변경
 
                 if(currentUser){
                     const userInfo = {
@@ -82,3 +82,27 @@ const SettingProfile = () => {
 };
 
 export default SettingProfile;
+
+/*
+
+배운점 
+
+1. const reader = new FileReader(); => 컴퓨터에 있는 파일을 읽을때 쓰는 객체 
+2. reader.onloadend = () => { ... }; => 파일을 다 읽었으면 어떤 로직을 실행해야하는지 
+3. const imgResult = reader.result; => 다 읽은 결과물을 저장 
+4. const imgResult = reader.result; => 여기에는 이미지 파일을 읽은 결과물이 들어간다. 이 변수안에는 이미지 파일이 엄청 긴 문자열 형태로 들어있다. 
+5. reader.readAsDataURL(file);
+
+6. 
+
+<input 
+    type="file" 
+    accept="image/*" 
+    onChange={onFileChange}
+    className="real-file-input"
+/>
+
+타입은 file이고 
+accept는 어떤 종류의 파일만 받을 것인지를 정해주는 속성이다. image/*이면 이미지 파일(png, jpg, jpeg, gif, webp 등)만 받겠다는 것이다. 
+이것은 완벽하지 않다 악의적인 유저가 개발자도구를 켜서 파일 종류를 바꿔버릴 수도 있기 때문이다. 그래서 백연동시에 이게 이미지 파일인지 확인하는 작업을 한번 더 해줘야 한다. 
+*/
