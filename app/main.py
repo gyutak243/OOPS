b7d3788bc2a962cbcd87b5655301a666cc39f4eb
심재서 <jaeseo0944@hufs.ac.kr>
2026-06-03 13:24:47 +0900

    fix: CORS에 localhost:5173 추가

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Oops Board API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
